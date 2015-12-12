b2912ed50b34d6359603116fcf0a527160950721
podyankee <you@gmail.com>
2015-12-12 03:31:24 +0200

    less to css comriling finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var config = require('./gulp.config')();
-
+var del = require('del');
 var $ = require('gulp-load-plugins')({lazy: true});
 
 gulp.task('vet', function() {
     log('Analyzing source with JSHint and JSCS');
 
     return gulp
-		.src(config.alljs)
+        .src(config.alljs)
         .pipe($.if(args.verbose, $.print()))
         .pipe($.jscs())
         .pipe($.jshint())
@@ -16,17 +16,32 @@ gulp.task('vet', function() {
         .pipe($.jshint.reporter('fail'));
 });
 
-gulp.task('styles', function  () {
-  log('Compiling Less --> CSS');
-	return gulp
-		.src(config.less)
-		.pipe($.less())
-		.pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
-		.pipe(gulp.dest(config.temp));
-  
+gulp.task('styles', function() {
+    log('Compiling Less --> CSS');
+
+    return gulp
+        .src(config.less)
+        .pipe($.plumber())
+        .pipe($.less())
+        .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean-styles', function(done) {
+    var files = config.temp + '**/*.css';
+    clean(files, done);
+});
+
+gulp.task('less-watcher', function() {
+    gulp.watch([config.less], ['styles']);
 });
 
 ////////////
+
+function clean(path, done) {
+    log('Cleaning: ' + $.util.colors.blue(path));
+    del(path, done);
+}
 
 function log(msg) {
     if (typeof(msg) === 'object') {
